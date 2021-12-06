f363c0f35ae6f5b6320765732cd56dcca258111f
blazinsk1n <kirilkostadinov@yahoo.com>
2021-12-06 15:42:15 +0200

    Register form update

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,60 +1,42 @@
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
 import TextField from '@mui/material/TextField';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import Button from '@mui/material/Button';
 
 export default function Register() {
+
+	const register = async (e) => {
+		e.preventDefault();
+
+		const { firstName, lastName, email } = Object.fromEntries(new FormData(e.currentTarget));
+
+		console.log(firstName, lastName, email)
+	}
+
 	return (
 		<Container sx={{ py: 5 }} maxWidth="sm">
-			<form>
+			<form onSubmit={register}>
 				<Grid container spacing={2}>
 					<Grid item xs={12} md={6}>
 						<TextField
-							id="standard-basic"
+							name='firstName'
 							label="First name"
 							variant="standard"
 							fullWidth />
 					</Grid>
 					<Grid item xs={12} md={6}>
 						<TextField
-							id="standard-basic"
+							name='lastName'
 							label="Last name"
 							variant="standard"
 							fullWidth />
 					</Grid>
-					<Grid item xs={12}>
+					<Grid item xs={12} sx={{ mb: 5 }}>
 						<TextField
-							id=""
+							name='email'
 							label="Email"
 							variant="standard"
 							fullWidth />
-					</Grid>
-					<Grid item xs={12}>
-						<FormControl fullWidth >
-							<InputLabel id="gender">Gender</InputLabel>
-							<Select
-								labelId="gender"
-								id="gender"
-								label="Gender"
-							>
-								<MenuItem>Male</MenuItem>
-								<MenuItem>Female</MenuItem>
-							</Select>
-						</FormControl>
-					</Grid>
-					<Grid item xs={12}>
-						<FormControlLabel
-							label="I agree with the Terms and Conditions"
-							control={
-								<Checkbox size="small" />
-							}
-						/>
 					</Grid>
 
 					<div className="btn-container">
