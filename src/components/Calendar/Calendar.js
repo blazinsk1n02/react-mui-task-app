86a95d6c8bcb2326b4f0d0f7f7c4407f0e4a17bf
blazinsk1n <kirilkostadinov@yahoo.com>
2021-12-30 23:40:48 +0200

    All tasks state refactoring

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -3,31 +3,23 @@ import TextField from '@mui/material/TextField';
 import StaticDatePicker from '@mui/lab/StaticDatePicker';
 import moment from 'moment';
 
-import { useState, useEffect } from 'react'
-import * as taskService from '../../services/taskService'
+import { useState } from 'react'
+import { useRecoilState } from "recoil";
+
+import { getAllState } from '../../atoms/getAllState'
 import styles from './Calendar.module.css'
 
 export default function Calendar() {
-  const [tasks, setTasks] = useState([]);
   const [date, setDate] = useState(new Date());
-
-  useEffect(() => {
-    taskService.getAll()
-      .then(result => {
-        setTasks(result);
-      });
-  }, []);
+  const [tasks, setTasks] = useRecoilState(getAllState);
 
   const onDateChange = (newDate) => {
-    const formatSelectedDate = moment(newDate).format('DD/MM/YYYY');
+    const formatSelectedDate = moment(newDate._d).format('DD/MM/YYYY');
 
-    const serverDateFormat = tasks.filter(x => {
+    const selectedDateTasks = tasks.filter(x => {
       return moment(x.created_at).format('DD/MM/YYYY') === formatSelectedDate
     });
-
-    console.log(serverDateFormat)
   };
-
 
   return (
     <div className={styles.calendar}>
